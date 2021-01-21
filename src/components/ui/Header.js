5423fad544e5f1da855ae49f7793f2b5c207f19b
zafaris <zafaris.dev@gmail.com>
2021-01-21 23:43:10 +0000

    Fixed header font size

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -4,9 +4,9 @@ import { faChess } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
-        <section>
+        <div>
             <h1><FontAwesomeIcon icon={faChess} /> Chess Stats</h1>
-        </section>
+        </div>
     )
 }
 
