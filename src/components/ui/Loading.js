1a92c8e767b265abf422f056b643e018334d61ad
zafaris <zafaris.dev@gmail.com>
2021-01-22 12:56:10 +0000

    Random loading chess icon

--- src/components/ui/Loading.js
+++ src/components/ui/Loading.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChessRook } from '@fortawesome/free-solid-svg-icons'
+import { faChessPawn, faChessKnight, faChessBishop, faChessRook, faChessQueen, faChessKing } from '@fortawesome/free-solid-svg-icons'
 
 const Loading = () => {
+    let icons = [faChessPawn, faChessKnight, faChessBishop, faChessRook, faChessQueen, faChessKing]
+
     return (
-        <FontAwesomeIcon icon={faChessRook} size="5x" style={{ "color": "#4F3BC8", "marginTop": "50px" }} spin />
+        <FontAwesomeIcon icon={icons[Math.floor(Math.random() * icons.length)]} size="5x" style={{ "color": "#4F3BC8", "marginTop": "50px" }} spin />
     )
 }
 
